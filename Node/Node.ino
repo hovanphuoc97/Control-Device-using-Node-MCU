3b684c67cd12eff2547fc558a2732c2b97f8716e
Phuoc Ho <hovanphuoc97@gmail.com>
2019-07-07 10:35:22 +0700

    mov

---
+++ Node/Node.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#define FIREBASE_HOST "pushdatatocontroldevice.firebaseio.com"
+#define FIREBASE_AUTH "r1stIhLEerpTimdPmcZaikZiuh1eMYDRcVptcvzx"
+#define WIFI_SSID "IoT-Research"
+#define WIFI_PASSWORD "Tapit168"
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  pinMode(16, OUTPUT);
+}
+void loop() {
+  // get value
+  String val = Firebase.getString("ESP8266/light/state");
+  Serial.println("state:" + val);
+  if (val == "on")
+  {
+    digitalWrite(16, LOW);
+  }
+  else if (val == "off")
+  {
+    digitalWrite(16, HIGH);
+  }
+  delay(1000);
+
+}
