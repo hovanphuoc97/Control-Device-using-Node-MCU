3e02aab7b5c3a10c56c449fb769c1d0a5b8b0667
Phuoc Ho <hovanphuoc97@gmail.com>
2019-07-22 14:12:26 +0700

    xxx

--- Node/usingStream/Firebase_ESP_Stream_Control_Led_JsonVariant.ino
+++ Node/usingStream/Firebase_ESP_Stream_Control_Led_JsonVariant.ino
@@ -1,11 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+
 #define LedPin 16
+
 // Set these to run example.
-#define FIREBASE_HOST "pushdatatocontroldevice.firebaseio.com"
-#define FIREBASE_AUTH "r1stIhLEerpTimdPmcZaikZiuh1eMYDRcVptcvzx"
-#define WIFI_SSID "Phan Gia Khai Main"
-#define WIFI_PASSWORD "thangnay30"
+#define FIREBASE_HOST "esp8266light.firebaseio.com"
+#define FIREBASE_AUTH "qRC0A4LpFBc4tWiulxijSQPwqqrvgetlBLWEHCMU"
+#define WIFI_SSID "IoT-Research"
+#define WIFI_PASSWORD "Tapit168"
 
 void setup() {
   pinMode(LedPin, OUTPUT);
@@ -21,28 +23,39 @@ void setup() {
   Serial.print("connected: ") ;
   Serial.println(WiFi.localIP());
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  Firebase.stream("/ESP8266/light/state");
+  Firebase.stream("/ESP8266");
 }
 void loop() {
   if (Firebase.failed()) {
     Serial.println("streaming error");
     Serial.println(Firebase.error());
   }
+
   if (Firebase.available()) {
     FirebaseObject event = Firebase.readEvent();
     String eventType = event.getString("type");
     String path = event.getString("path");
-    String data = event.getString("data");
+    String data;
     Serial.println(String("event: ") + eventType);
     Serial.println(String("path: ") + path);
-    JsonVariant getJsonVariant(data\)const
-    if (data == "off")
-    {
-      digitalWrite(LedPin, HIGH);
+    JsonVariant jsonVariant = event.getJsonVariant("data");
+    jsonVariant.prettyPrintTo(data);
+    Serial.println(String("data: ") + data);
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.parseObject(data);
+    if (!root.success()) {
+      Serial.println(F("Parsing failed!"));
+      return;
     }
-    else if (data  == "on")
-    {
-      digitalWrite(LedPin, LOW);
+    if (path == "/light") {
+      String state = root["state"];
+      Serial.println(String("state: ") + state);
+      if (state == "on") {
+        digitalWrite(LedPin, LOW);
+      }
+      else if (state == "off") {
+        digitalWrite(LedPin, HIGH);
+      }
     }
   }
 }
